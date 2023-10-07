ac9e0a1b19f43022e0740ee18f20775dcbbb9736
aa-r-ju <aarju3447@gmail.com>
2023-10-07 16:38:00 +0545

    feat:completed part9 c video b

--- flight_dairy/src/index.ts
+++ flight_dairy/src/index.ts
@@ -1,4 +1,5 @@
 import express from 'express';
+import router from "./routes/diaries";
 const app = express();
 app.use(express.json());
 
@@ -8,6 +9,9 @@ app.get('/ping', (_req, res) => {
   console.log('someone pinged here');
   res.send('pong');
 });
+
+app.use("/api/diaries",router);
+
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
