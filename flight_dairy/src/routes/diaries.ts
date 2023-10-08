25cb5987a5825d54d47502eb19098795706f51ea
aa-r-ju <aarju3447@gmail.com>
2023-10-08 22:55:27 +0545

    feat:completed part9 d video1

--- flight_dairy/src/routes/diaries.ts
+++ flight_dairy/src/routes/diaries.ts
@@ -1,13 +1,29 @@
+/* eslint-disable @typescript-eslint/no-unsafe-assignment */
 import express from 'express';
 import diaryService from '../services/diaryService';
+import toNewDiaryEntry from '../services/utils';
 const router = express.Router();
 
 router.get('/', (_req, res) => {
   res.send(diaryService.getNonSensitiveEntries());
 });
 
-router.post('/', (_req, res) => {
-  res.send('Saving a diary!');
+
+router.get('/:id', (req, res) => {
+  const diary = diaryService.findById(Number(req.params.id));
+
+  if (diary) {
+    res.send(diary);
+  } else {
+    res.sendStatus(404);
+  }
 });
 
+
+
+router.post('/', (req, res) => {
+  const newDiaryEntry= toNewDiaryEntry(req.body);
+  const addedEntry = diaryService.addDiary(newDiaryEntry);
+  res.json(addedEntry);
+});
 export default router;
